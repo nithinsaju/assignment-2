4aa76c18fcf638157ec7cf2a78a1b66e5352c376
nithinsaju <51135252+nithinsaju@users.noreply.github.com>
2021-10-10 21:01:43 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  type        = string
+  description = "Project name"
+}
+
+variable "region" {
+  type        = string
+  description = "Azure region"
+  default     = "us-east-1"
+}
+	
